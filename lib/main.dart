7206d18fac9071e1db1e3e723d6eacbb80b8f670
seryxme <coolbams24@yahoo.com>
2022-12-08 15:58:48 +0100

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'IntelliApp Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.cyanAccent,
+          foregroundColor: Colors.blueGrey,
+        ),
+      ),
+      home: const RandomWords(),
     );
   }
 }
